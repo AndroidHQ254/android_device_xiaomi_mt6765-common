fca0a98e02801066b1df96cf3d726081ad593b25
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-12-16 18:30:47 +0000

    up

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
-# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x11a88000 --second_offset 0x00e88000 --tags_offset 0x07808000
